bff567d251b6460b706dfe4199c29d78c50ffd86
MubashirWaheed <mubashir3080@gmail.com>
2022-01-07 13:43:01 +0500

    completed testimonial section

---
+++ scrip.js
@@ -0,0 +1,13 @@
+
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: 'bullets',
+    // dynamicBullets: true,
+  },
+});
