9b8f70df095b4bc581a984e2fd4c08b887ade79c
MubashirWaheed <mubashir3080@gmail.com>
2022-01-09 19:18:12 +0500

    made responsive

--- scrip.js
+++ scrip.js
@@ -1,4 +1,4 @@
-
+// Slider using swipper.js library 
 var swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 3000,
@@ -8,7 +8,20 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     type: 'bullets',
-    // dynamicBullets: true,
   },
 });
 
+// Hamburger 
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click',()=>{
+  hamburger.style.display = "none"
+  menu.style.display = 'block';
+})
+
+close.addEventListener('click',()=>{
+  hamburger.style.display = "block"
+  menu.style.display = 'none';
+})
